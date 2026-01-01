4c6817f094bed246e1cf4c8334f5e4cab1cecc98
Vladimir Shabanov <vavanrrrage@gmail.com>
2026-01-01 23:54:07 +0300

    feat: add logging for requests and errors

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -3,6 +3,18 @@ import { auth } from "../auth";
 
 const app = new Elysia()
   .mount(auth.handler)
+  .onRequest(({ request }) => {
+    console.log(
+      `[${new Date().toISOString()}] ${request.method} ${request.url}`,
+    );
+  })
+  .onError(({ request, error }) => {
+    console.group(
+      `[${new Date().toISOString()}] ${request.method} ${request.url}`,
+    );
+    console.log(`ERROR: ${String(error)}`);
+    console.groupEnd();
+  })
   .listen(3000);
 
 console.log(
